b58cdc96549a058636d510f237ed231f2d38c933
Rodrigo Ramírez López <cientoundalmatas693@gmail.com>
2024-04-18 10:51:51 +0200

    -- Save
    - Create packages to order the proyect.

---
+++ Java-Swing-App/src/main/java/com/mycompany/java/swing/app/JavaSwingApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java.swing.app;
+
+/**
+ *
+ * @author Alumno
+ */
+public class JavaSwingApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
