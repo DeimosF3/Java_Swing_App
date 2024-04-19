8c10a8491744def878c2621c795f36590653a93c
Jkxkds <108697221+Jkxkds@users.noreply.github.com>
2024-04-19 18:58:42 +0200

    18:58/19-04-2024 le queda poquito a la consulta

--- Java-Swing-App/src/main/java/com/mycompany/classes/bbdd/AnimalDAO.java
+++ Java-Swing-App/src/main/java/com/mycompany/classes/bbdd/AnimalDAO.java
@@ -3,29 +3,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.classes.bbdd;
-
 import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 /**
  *
  * @author Alumno
  */
-public class AnimalDAO {
-//    public void insertarFabricante(Fabricante f) throws SQLException {
-//        Statement s = new conexion().getconexion().createStatement();
-//        int nFil = s.executeUpdate("insert into fabricante(id,nombre) values (default,'" + f.getNombre() + "')");
-//        new conexion().getconexion().createStatement().executeUpdate("insert into fabricante(id,nombre) values (default,'" + f.getNombre() + "')");
-//    }
-    public void insertarAnimal(String tipo, String nombre, String especie, String peso, String lesion, String gravedad, Boolean cazaFurtiva) throws Exception {
-//        
+public class AnimalDAO {    
+    
+    public void insertarAnimal(String tipo, String nombre, String especie, String peso, String lesion, String gravedad, boolean cazaFurtiva) throws Exception {
+       new conexion().getconexion().createStatement().executeUpdate("INSERT INTO animales (tipo, nombre, especie, peso, tipo_lesion, gravedad, motivo_lesion) "
+                + "VALUES ('" + tipo + "', '" + nombre + "', '" + especie + "', " + peso + ", '" + lesion + "', '" + gravedad + "', '" + cazaFurtiva + "')");
+
     }
 }
-/*
--- Insertar nuevos registros en la tabla `animales`
-INSERT INTO `animales` (`tipo`, `nombre`, `especie`, `peso`, `tipo_lesion`, `gravedad`, `motivo_lesion`, `infeccion_bacteriana`)
-VALUES
-('ave', 'Piolín', 'Canario', '0.05', 'Fractura de ala', '2', 'Caza', '0'),
-('mamífero', 'Bambi', 'Ciervo', '60.00', 'Herida leve', '1', 'Caza', '0'),
-('reptil', 'Godzilla', 'Iguana marina', '8.00', 'Fractura de cola', '2', NULL, '0');
-
-*/
