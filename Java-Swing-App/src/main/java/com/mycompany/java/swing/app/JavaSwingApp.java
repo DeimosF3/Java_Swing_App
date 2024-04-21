dc782fa604283b5923c97948239237c393a209ca
DeimosF3 <cientoundalmatas693@gmail.com>
2024-04-22 01:41:22 +0200

    ahora el programa se ejecuta desde app

--- Java-Swing-App/src/main/java/com/mycompany/java/swing/app/JavaSwingApp.java
+++ Java-Swing-App/src/main/java/com/mycompany/java/swing/app/JavaSwingApp.java
@@ -1,8 +1,10 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.java.swing.app;
+
+import com.mycompany.gui.panell;
+import com.mycompany.guiUtils.Utilities;
 
 /**
  *
@@ -11,6 +13,14 @@ package com.mycompany.java.swing.app;
 public class JavaSwingApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        panell asd = new panell();
+        asd.setVisible(true);
+        asd.setLocationRelativeTo(null);
+        asd.setResizable(false);
+        asd.setTitle("Centro de Fauna");
+
+        Utilities u = new Utilities();
+
+        u.setFontRecursively(asd.getContentPane());
     }
 }
